2bfbea5e3c87da8855002157e79b9d14620c4761
Barbara Pusnar <barbara.pusnar@tsc.si>
2018-01-21 08:15:00 +0100

    Prvo shranjevanje

---
+++ Test3/Test3/Controllers/MojController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test3.Controllers
+{
+    public class MojController : Controller
+    {
+        // GET: Moj
+        public ActionResult Index()
+        {
+            data podatki = Helper.Beri();
+
+            if (podatki != null)
+            {
+                return View(podatki);
+            }
+            else
+            {
+                podatki = new data();
+                return View(podatki);
+            }
+        }
+    }
+}
---
+++ Test3/Test3/Helper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Xml.Serialization;
+using System.Net.Http;
+using System.Xml;
+
+namespace Test3
+{
+    public class Helper
+    {
+        
+        public static data  Beri()
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(
+               new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/xml"));
+
+            HttpResponseMessage response = client.GetAsync(new Uri("http://www.meteo.si/uploads/probase/www/observ/surface/text/sl/recent/observationAms_NOVA-GOR_history.xml")).Result;
+            if (response.IsSuccessStatusCode)
+            {
+
+                var x =  response.Content.ReadAsStreamAsync().Result;
+                XmlSerializer ser = new XmlSerializer(typeof(data));
+                Stream rawXml = x;
+                var p = ser.Deserialize(rawXml) as data;
+                return p;
+            }
+            else
+            { return null; }
+        }
+       
+    }
+}
